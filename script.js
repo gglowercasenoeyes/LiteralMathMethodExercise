c008fdb61dc2033b7359545172eda7d6aeb9de83
“username” <angeljunotruffles@gmail.com>
2022-03-08 23:47:44 -0500

    Look at the last one.

---
+++ script.js
@@ -0,0 +1,21 @@
+let warmHugs = 'Hi, I\'m Olaf and I like warm hugs.';
+warmHugs = warmHugs.replace('like', 'love');
+console.log(warmHugs.toUpperCase());
+
+let beenImpaled = 'Oh, look at that. I\'ve been impaled';
+console.log(beenImpaled.slice(18,36));
+
+const dotDotDot = '...';
+let skullBones = `I don\'t have a skull${dotDotDot}nor any bones.`;
+console.log(skullBones); 
+
+console.log(Math.PI);
+
+console.log(Math.ceil(Math.random()*3));
+
+console.log(' Let It Go!'.repeat(2).trim());
+
+const reindeers = 'Reindeers are better than people.';
+console.log(reindeers.replace(/\s/g, "_"));
+
+console.log(Math.SQRT2);
